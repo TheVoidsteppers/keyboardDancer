bdf352304b3120645b3ca35ee6c83e91f34a8e26
TheVoidsteppers <15659974720@163.com>
2024-05-13 22:38:36 +0800

    feat: 添加快捷console.log方法

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,42 @@ import { ISetting } from './settings';
 import * as rjson from 'relaxed-json';
 import { resolve } from 'path';
 
+interface WrapData {
+  txt: string;
+  item: string;
+  sel: vscode.Selection;
+  doc: vscode.TextDocument;
+  ran: vscode.Range;
+  ind: string;
+  idx: number;
+  line: number;
+  lastLine: boolean;
+}
+
+enum Wrap {
+  Inline = 'Inline',
+  Down = 'Down',
+  Up = 'Up',
+}
+
+enum FormatAs {
+  String = 'String',
+}
+
+let currentEditor: vscode.TextEditor;
+
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+  currentEditor = vscode.window.activeTextEditor!;
   context.globalState.setKeysForSync(['keyboardDancerVersion']);
-  context.subscriptions.push(vscode.commands.registerCommand('extension.importFromKeyboardDancer', () => start()));
+  // 切换编辑器tab时，改变 currentEditor
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => (currentEditor = editor!)
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.keyboardDancer.importFromKeyboardDancer', () => start()),
+    vscode.commands.registerCommand('extension.keyboardDancer.console.log.variable', () => handleWrap(Wrap.Down)),
+    vscode.commands.registerCommand('extension.keyboardDancer.console.log.string', () => handleWrap(Wrap.Down, FormatAs.String)),
+  );
   const lastVersion = context.globalState.get('keyboardDancerVersion') || '';
   const currentVersion = context.extension.packageJSON.version
   if (lastVersion !== currentVersion) {
@@ -71,4 +104,151 @@ async function importSettings(settings: ISetting[]): Promise<void> {
       }
     }
   });
+}
+
+function getSetting(setting: string) {
+  var spl = setting.split(".");
+
+  return spl.length == 1
+    ? vscode.workspace.getConfiguration("keyboard-dancer")[setting]
+    : spl.splice(1).reduce((a, b) => {
+      return a[b];
+    }, vscode.workspace.getConfiguration("keyboard-dancer")[spl[0]]);
+}
+
+async function handleWrap(
+  target: Wrap,
+  formatAs?: FormatAs
+) {
+  new Promise<WrapData>((resolve, reject) => {
+    let sel = currentEditor.selection;
+    let len = sel.end.character - sel.start.character;
+
+    let ran =
+      len == 0
+        ? currentEditor.document.getWordRangeAtPosition(sel.anchor)
+        : new vscode.Range(sel.start, sel.end);
+
+    if (ran == undefined) {
+      reject("NO_WORD")
+      return
+    }
+    let doc = currentEditor.document;
+    let lineNumber = ran.start.line;
+    let idx = doc.lineAt(lineNumber).firstNonWhitespaceCharacterIndex;
+    let wrapData: WrapData = {
+      txt: '',
+      item: doc.getText(ran),
+      doc: doc,
+      ran: ran,
+      idx: idx,
+      ind: doc.lineAt(lineNumber).text.substring(0, idx),
+      line: lineNumber,
+      sel: sel,
+      lastLine: doc.lineCount - 1 == lineNumber,
+    };
+
+    if (formatAs != undefined) {
+      switch (formatAs) {
+        case FormatAs.String:
+          wrapData.txt = getSetting("console.log.functionName").concat(
+            "('",
+            wrapData.item,
+            "')"
+          );
+          break;
+      }
+    } else {
+      wrapData.txt = getSetting("console.log.format")
+        .replace("$functoin", getSetting("console.log.functionName"))
+        .replace(/[$]var/g, wrapData.item);
+    }
+    resolve(wrapData);
+  })
+    .then((wrap: WrapData) => {
+      function SetCursor(line: number) {
+        let tpos = new vscode.Position(
+          line,
+          currentEditor.selection.anchor.character
+        )
+        if (tpos) {
+          currentEditor.selection = new vscode.Selection(tpos, tpos);
+        }
+      }
+
+      switch (target) {
+        case Wrap.Down: {
+          let nxtLine: vscode.TextLine;
+          let nxtLineInd: string;
+
+          if (!wrap.lastLine) {
+            nxtLine = wrap.doc.lineAt(wrap.line + 1);
+            nxtLineInd = nxtLine.text.substring(
+              0,
+              nxtLine.firstNonWhitespaceCharacterIndex
+            );
+          } else {
+            nxtLineInd = "";
+          }
+
+          wrap.ind = "";
+          new vscode.Position(
+            wrap.line,
+            wrap.doc.lineAt(wrap.line).range.end.character
+          );
+
+          currentEditor
+            .edit((e) => {
+
+              if (wrap.lastLine == false && nxtLine.isEmptyOrWhitespace) {
+                function defaultAction() {
+                  e.insert(
+                    new vscode.Position(
+                      wrap.line,
+                      wrap.doc.lineAt(wrap.line).range.end.character
+                    ),
+                    "\n".concat(
+                      nxtLineInd > wrap.ind ? nxtLineInd : wrap.ind,
+                      wrap.txt
+                    )
+                  );
+                }
+                defaultAction();
+              } else {
+                e.insert(
+                  new vscode.Position(
+                    wrap.line,
+                    wrap.doc.lineAt(wrap.line).range.end.character
+                  ),
+                  "\n".concat(
+                    nxtLineInd.length > wrap.ind.length ? nxtLineInd : wrap.ind,
+                    wrap.txt
+                  )
+                );
+              }
+            })
+            .then(() => {
+              if (nxtLine == undefined) {
+                nxtLine = wrap.doc.lineAt(wrap.line + 1);
+              }
+              if (!wrap.lastLine) {
+                let nextLineEnd = wrap.doc.lineAt(wrap.line + 2).range.end;
+                currentEditor.selection = new vscode.Selection(
+                  wrap.sel.start,
+                  nextLineEnd
+                );
+              } else {
+                currentEditor.selection = wrap.sel;
+              }
+              SetCursor(wrap.line);
+            });
+        }
+          break;
+        default:
+          break;
+      }
+    })
+    .catch((message) => {
+      console.log("vscode-keyboard-dancer console log CANCEL: " + message);
+    });
 }
